d50d5aa3588714ee7d7ee44cc3ad781828ebc2a5
Ryan Freude <freudehack@gmail.com>
2017-06-21 14:45:09 -0500

    API requests are working and todays temperature data is being displayed

--- weathermod/src/App.jsx
+++ weathermod/src/App.jsx
@@ -1,35 +1,105 @@
 import React, { Component } from 'react';
+import moment from 'moment';
 import './App.css';
 import Today from './Today.jsx';
-import Outlook from './Outlook.jsx';
+import Outlook from './Outlook';
 import $ from 'jquery';
 
 class App extends Component {
   constructor (props) {
     super(props);
 
+    this.state = {
+      highTemp: null,
+      lowTemp: null,
+      date: moment().format('LLL'),
+      currTemp: null,
+      currWeather: "",
+      currCity: "New York",
+      displayCity: "New York"
+    }
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+
   }
 
+  componentDidMount() {
+    this.currentWeatherCall();
+    this.dailyForcastCall();
+  }
 
+  handleClick() {
+    this.componentDidMount();
+    this.setState({displayCity: this.state.currCity});
+    this.setState({currCity: ''})
+  }
 
-  weatherCall() {
+  handleInputChange(event) {
+    let input = event.target.value;
+
+    this.setState({currCity: input})
+
+  }
+
+  currentWeatherCall() {
     const API_KEY = "307baac004d25e4d4ec945d7c93cf259";
-    return $.getJSON('http://api.openweathermap.org/data/2.5/forecast?q=appleton,us&units=imperial&APPID=' + API_KEY)
+
+    return $.getJSON('http://api.openweathermap.org/data/2.5/forecast?q='+ this.state.currCity +',us&units=imperial&APPID=' + API_KEY)
       .then((data) => {
-        console.log(data);
+        this.setState({
+          currTemp: data.list[0].main.temp,
+          currWeather: data.list[0].weather[0].main
+        })
+      });
+  }
+
+  dailyForcastCall() {
+    const API_KEY = "307baac004d25e4d4ec945d7c93cf259";
+
+    return $.getJSON('http://api.openweathermap.org/data/2.5/forecast/daily?q='+ this.state.currCity +',us&units=imperial&APPID=' + API_KEY)
+      .then((data) => {
+        this.setState({
+          highTemp: data.list[0].temp.max,
+          lowTemp: data.list[0].temp.min
+        })
       });
   }
 
   render() {
     return (
       <div className="weather-main">
+        <div className="input-group">
+          <input
+            placeholder="Search for..."
+            ref="cityInput"
+            value={this.state.currCity}
+            onChange={this.handleInputChange}
+            type="text"
+            className="form-control"
+          />
+          <span className="input-group-btn">
+          <button
+            className="btn btn-default"
+            type="button"
+            onClick={this.handleClick}>
+            Search
+          </button>
+          </span>
+        </div>
         <div className="today-main">
           <img src="http://via.placeholder.com/200x200" alt="..." />
-          <Today />
+          <Today
+            currTemp={ Math.round(this.state.currTemp) }
+            currWeather= {this.state.currWeather}
+            currCity={this.state.displayCity}
+            highTemp={this.state.highTemp}
+            lowTemp={this.state.lowTemp}
+            date={this.state.date}
+          />
         </div>
         <div className="outlook-main">
           <Outlook />
-          {this.weatherCall()}
         </div>
       </div>
     );
--- weathermod/src/Today.jsx
+++ weathermod/src/Today.jsx
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
+
 import './App.css';
 
 class Today extends Component {
   render() {
     return (
       <div className="today-forcast">
-        <p className="today-temp">100 F</p>
-        <p className="today-weather">Partly Cloudy</p>
+        <p className="today-city">{this.props.currCity}</p>
+        <p className="today-date">{this.props.date}</p>
+        <div>
+          <p className="today-temp-curr">{this.props.currTemp}</p>
+          <div className="today-extremes">
+            <div className="today-extreme-high">
+              <p className="today-temp-high">{Math.round(this.props.highTemp)}</p>
+              <p className="desc-text">high</p>
+            </div>
+            <div  className="today-extreme-low">
+              <p className="today-temp-low">{Math.round(this.props.lowTemp)}</p>
+              <p className="desc-text">low</p>
+            </div>
+          </div>
+        </div>
+        <p className="today-weather">{this.props.currWeather}</p>
       </div>
     );
   }
